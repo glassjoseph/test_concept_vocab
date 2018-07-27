731440b9542c6c4fe16f8c4ee6b9a62405c92d21
glassjoseph <jhg2v@virginia.edu>
2018-07-27 17:06:43 -0400

    Add repository functions

--- src/Repository/ConceptRepository.php
+++ src/Repository/ConceptRepository.php
@@ -6,6 +6,10 @@ use App\Entity\Concept;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
+// use Doctrine\DBAL\Driver\Connection; // which one?
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityRepository;
+
 /**
  * @method Concept|null find($id, $lockMode = null, $lockVersion = null)
  * @method Concept|null findOneBy(array $criteria, array $orderBy = null)
@@ -20,6 +24,69 @@ class ConceptRepository extends ServiceEntityRepository {
     // public function findAllWithTerms() {
     // }
 
+
+    /**
+     * @return Concept[] Returns an array of Concept objects
+     */
+    public function findByUrl($value) {
+        return $this->createQueryBuilder('t')
+            ->andWhere('lower(t.url) LIKE lower(:value)')
+            ->setParameter('value', $value)
+            ->setMaxResults(20)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+
+
+
+    public function getPreferredTerms() {
+
+        $sql = "SELECT c.id, t.value FROM concept c LEFT JOIN term t ON c.id = t.concept_id and t.preferred = 't'";
+
+        // return $this->createQueryBuilder()->select('c.id')->from('concept', 'c')->innerJoin('c', 'term', 't', 'c.id = t.concept_id')->getQuery()->getResult();
+
+
+        $conn = $this->getEntityManager()->getConnection();
+        $conn->createQueryBuilder()->select('c.id')->from('concept', 'c')->innerJoin('c', 'term', 't', 'c.id = t.concept_id');
+
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+
+
+    public function getPreferredTermsSQL() {
+
+        $sql = "SELECT * FROM concept c LEFT JOIN term t ON c.id = t.concept_id and t.preferred = 't'";
+        //or
+        $sql = $conn->createQueryBuilder()->select('c.id')->from('concepts', 'c')->innerJoin('c', 'terms', 't', 'c.id = t.concept_id');
+
+
+        $conn = $this->getEntityManager()->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    // public function getPreferredTermsSQL() {
+    //         require('/Users/josephglass/.composer/vendor/autoload.php');
+    //         \Psy\Shell::debug(get_defined_vars(), $this);
+    //
+    //     return $this->createQueryBuilder('c')
+    //     $sql = "SELECT * FROM concept c LEFT JOIN term t ON c.id = t.concept_id and t.preferred = 't'";
+    //
+    //     $conn = $this->getDoctrine()->getEntityManager()->getConnection();
+    //
+    //     $stmt = $conn->getConnection()->prepare($sql);
+    //     $stmt->execute();
+    //     return $stmt->fetchAll();
+    // }
+    //
 
 
 //    /**
